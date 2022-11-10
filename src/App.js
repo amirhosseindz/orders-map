c617916ebfee214d4ea941c9227a4071b90465ce
Amir <amirhosse.dashtizad@check24.de>
2022-11-10 10:37:45 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+import Cart from "./components/Cart"
+import Map from "./components/Map"
+
+function App() {
+    return (
+        <div className="App">
+            <Cart/>
+            <Map/>
+        </div>
+    )
+}
+
+export default App;
